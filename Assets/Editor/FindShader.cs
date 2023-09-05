742203cf2725ed95f5024ef7c6d4e959fd97e624
EnderPurrson <enderlord9990@gmail.com>
2023-09-05 03:15:27 -0400

    fixed wind shader

---
+++ Assets/Editor/FindShader.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEditor;
+
+public class FindShader : Editor
+{
+	//[MenuItem("Shader Test/Find The WIND!!")]
+	//public static void FindTheWind()
+	//{
+	//	FindTheShader("Triniti/Environment/WIND_COL_AB_2S_DO");
+	//}
+
+	public static void FindTheShader(string shader)
+	{
+		foreach (Material mat in AllMaterials)
+		{
+			if (mat.shader.name == shader)
+			{
+				Debug.LogError(mat.name);
+			}
+		}
+	}
+
+	public static List<Material> AllMaterials
+	{
+		get
+		{
+			return (from guid in AssetDatabase.FindAssets("t:Material") select AssetDatabase.LoadAssetAtPath<Material>(AssetDatabase.GUIDToAssetPath(guid))).ToList();
+		}
+	}
+}
